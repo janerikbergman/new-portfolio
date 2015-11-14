4825846d9694426fab467a2cdb29e0b18d1bbcf3
Janu <jan-erik.bergman@outlook.com>
2015-11-14 16:35:33 +0200

    Basic template for the portfolio.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function (){
+  /*$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p2").offset().top
+    }, 2000);
+  });*/
+  
+});
