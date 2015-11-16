681905fb0246e01f1b5a19814bdeb3d0d3018499
Janu <jan-erik.bergman@outlook.com>
2015-11-16 11:30:44 +0200

    Updated basic layout for the site and added javascript functionality.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 // JavaScript Document
 $(document).ready(function (){
 
+  $("#infoButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section1").offset().top - $("#header").height()
+    }, 1000);
+  });
+  $("#educationButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section2").offset().top  - $("#header").height()
+    }, 1000);
+  });
+  $("#workButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section3").offset().top - $("#header").height()
+    }, 1000);
+  });
+
  /*$(function(){
  var shrinkHeader = 100;
   $(window).scroll(function() {
@@ -15,6 +31,6 @@ $(document).ready(function (){
   function getCurrentScroll() {
       return window.pageYOffset;
       }
-  });  */
+  }); */ 
   
 });
