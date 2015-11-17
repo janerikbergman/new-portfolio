663c4df4793e5e318a7e3a6662e89c1f08516668
Janu <jan-erik.bergman@outlook.com>
2015-11-17 11:57:29 +0200

    Updated the CSS and added basic javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,36 +1,80 @@
 // JavaScript Document
 $(document).ready(function (){
 
-  $("#infoButton").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#section1").offset().top - $("#header").height()
-    }, 1000);
-  });
-  $("#educationButton").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#section2").offset().top  - $("#header").height()
-    }, 1000);
-  });
-  $("#workButton").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#section3").offset().top - $("#header").height()
-    }, 1000);
+  var headerHeight = $("#header").height();
+  var documentScrollTop = $(document).scrollTop();
+  var section1ScrollTop = Math.round($("#section1").offset().top)-headerHeight;
+  var section2ScrollTop = Math.round($("#section2").offset().top)-headerHeight;
+  var section3ScrollTop = Math.round($("#section3").offset().top)-headerHeight;
+  
+  $(window).resize(function () {
+    headerHeight = $("#header").height();
+    documentScrollTop = $(document).scrollTop();
+    section1ScrollTop = Math.round($("#section1").offset().top)-headerHeight;
+    section2ScrollTop = Math.round($("#section2").offset().top)-headerHeight;
+    section3ScrollTop = Math.round($("#section3").offset().top)-headerHeight;
   });
 
- /*$(function(){
- var shrinkHeader = 100;
   $(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
-           $("#header").addClass("shrink");
-        }
-        else {
-            $("#header").removeClass("shrink");
-        }
+  if($(document).scrollTop()==section1ScrollTop)
+  {
+    $("#infoButton").prop("disabled", true);
+    $("#educationButton").prop("disabled", false);
+    $("#workButton").prop("disabled", false);
+  }
+  else if($(document).scrollTop()==section2ScrollTop)
+  {
+    $("#infoButton").prop("disabled", false);
+    $("#educationButton").prop("disabled", true);
+    $("#workButton").prop("disabled", false);
+  }
+  else if($(document).scrollTop()==section3ScrollTop)
+  {
+    $("#infoButton").prop("disabled", false);
+    $("#educationButton").prop("disabled", false);
+    $("#workButton").prop("disabled", true);
+  }
   });
-  function getCurrentScroll() {
-      return window.pageYOffset;
-      }
-  }); */ 
+
+    
+
+  $("#infoButton").click(function() {
+    if ($('html, body').is(':animated'))
+    {
+      return false;
+    }
+    else
+    {
+      $('html, body').animate({
+          scrollTop: Math.round($("#section1").offset().top) - $("#header").height()
+      }, 1000);
+    }
+  });
+    
+  $("#educationButton").click(function() {
+    if ($('html, body').is(':animated'))
+    {
+      return false;
+    }
+    else
+    {
+      $('html, body').animate({
+          scrollTop: Math.round($("#section2").offset().top) - $("#header").height()
+      }, 1000);
+    }
+  });
+  
+  $("#workButton").click(function() {
+    if ($('html, body').is(':animated'))
+    {
+      return false;
+    }
+    else
+    {
+      $('html, body').animate({
+          scrollTop: Math.round($("#section3").offset().top) - $("#header").height()
+      }, 1000);
+    }
+  });
   
 });
