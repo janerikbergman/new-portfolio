f9bdc51a457e79ecef2dff2d0e6e171c3e96c312
Janu <jan-erik.bergman@outlook.com>
2015-12-01 11:23:44 +0200

    Refined the script.

--- js/languageScript.js
+++ js/languageScript.js
@@ -1,51 +1,53 @@
 // JavaScript Document
 angular.module("translationApp", [])
 .controller("translationCtrl", ["$scope", function($scope) {
-  $scope.content = 
+  $scope.partials = 
     [ { fin: "./partials/info_fi.html", en: "./partials/info_en.html"},
       { fin: "./partials/education_fi.html", en: "./partials/education_en.html"},
       { fin: "./partials/work_fi.html", en: "./partials/work_en.html"},
       { fin: "./partials/project_fi.html", en: "./partials/project_en.html"}, ];
   
-  $scope.buttons = [{ fin: "Tietoa", en: "About Me"},
+  $scope.text = [{ fin: "Tietoa", en: "About Me"},
       { fin: "Koulutus", en: "Education"},
-      { fin: "Työnäytteet", en: "Work"},];
-       
-  $scope.info = $scope.content[0].fin;
-  $scope.education = $scope.content[1].fin;
-  $scope.work = $scope.content[2].fin;
-  $scope.project = $scope.content[3].fin;
-  $scope.buttonText = "English";
+      { fin: "Työnäytteet", en: "Work"},
+      { fin: "Taidot", en: "Skills"},
+      { fin: "RESPONSIIVISUUS", en: "RESPONSIVE DESIGN"},
+      { fin: "English", en: "Suomi"}];
   
-  $scope.infoButton = $scope.buttons[0].fin;
-  $scope.educationButton = $scope.buttons[1].fin;
-  $scope.workButton = $scope.buttons[2].fin;
+  $scope.sections = [];
+  $scope.texts = [];
   
-  var flag = true;
+  var countPartials = $scope.partials.length;
+  var countTexts = $scope.text.length;   
+
+  $scope.flag = true;
+
+  $scope.getContent = function(){
+    if($scope.flag===true)
+    {
+        for(i = 0; i<countPartials; i++){
+          $scope.sections[i] = $scope.partials[i].fin;
+        }
+        for(i = 0; i<countTexts; i++){
+          $scope.texts[i] = $scope.text[i].fin;
+        }
+        $scope.flag = false;
+    }
+    else if($scope.flag===false)
+    {
+        for(i = 0; i<countPartials; i++){
+          $scope.sections[i] = $scope.partials[i].en;
+        }
+        for(i = 0; i<countTexts; i++){
+          $scope.texts[i] = $scope.text[i].en;
+        }
+        $scope.flag = true;
+    }
+  }
+  
+  $scope.getContent();
   
   $scope.changeContent = function(){
-      if(flag===true)
-      {
-        $scope.info = $scope.content[0].en;
-        $scope.education = $scope.content[1].en;
-        $scope.work = $scope.content[2].en;
-        $scope.project = $scope.content[3].en;
-        $scope.infoButton = $scope.buttons[0].en;
-        $scope.educationButton = $scope.buttons[1].en;
-        $scope.workButton = $scope.buttons[2].en;
-        $scope.buttonText = "Suomi";
-        flag = false;
-      }else if(flag===false)
-      {
-        $scope.info = $scope.content[0].fin;
-        $scope.education = $scope.content[1].fin;
-        $scope.work = $scope.content[2].fin;
-        $scope.project = $scope.content[3].fin;
-        $scope.infoButton = $scope.buttons[0].fin;
-        $scope.educationButton = $scope.buttons[1].fin;
-        $scope.workButton = $scope.buttons[2].fin;
-        $scope.buttonText = "English";
-        flag = true;
-      }
+      $scope.getContent();      
   }
 }]);
