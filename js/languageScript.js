f990b6fcac8d1a6be340f19c72170caf1a0de0f0
Janu <jan-erik.bergman@outlook.com>
2015-12-18 12:08:10 +0200

    Added HTTP get for a JSON file and moved logic into factories. Also the skill table content is now retrieved from the JSON file.

--- js/languageScript.js
+++ js/languageScript.js
@@ -17,14 +17,145 @@ app.factory("localStorageFactory", function localStorageFactory() {
   return localStorageFactory;
 });
 
-app.controller("translationCtrl", ["$scope", "localStorageFactory", function($scope, localStorageFactory) {
-  $scope.partials = 
-    [ { fin: "./partials/info_fi.html", en: "./partials/info_en.html"},
+app.factory("httpFactory", ["$http",function($http){
+    var data;
+    return {
+        getData: function (callback) {
+            if(data) {
+                callback(data);
+            } else {
+                $http.get("./js/data.json").success(function(d) {
+                    callback(data = d);
+                });
+            }
+        }
+    }; 
+}]);
+
+app.factory("heartFactory", ["$sce", function($sce){
+    var heartFactory = {};
+    var hearts = [];
+    
+    var heart = '<img src="./images/heart_small.png" height="27" width="30" alt="heart">';
+    var heartHalf = '<img src="./images/hearthalf_small.png" height="27" width="30" alt="heart">';
+    var heartEmpty = '<img src="./images/heartempty_small.png" height="27" width="30" alt="heart">';
+    
+    heartFactory.addHearts = function (index, value){
+      hearts[index] = "";
+      
+      for(i = 0; i < 5; i++){
+         if(value > i && !(value < i+1))
+         {
+            hearts[index]=$sce.trustAsHtml(hearts[index]+ heart);
+         }
+         else if (value > i && value < i+1)
+         {
+           hearts[index]=$sce.trustAsHtml(hearts[index]+ heartHalf); 
+         }
+         else {
+            hearts[index]=$sce.trustAsHtml(hearts[index]+ heartEmpty);
+         }
+          
+      } 
+
+      return hearts[index];
+    }
+    return heartFactory;
+}]);
+
+app.factory("languageFactory", function(){
+    var languageFactory = {};
+    
+    languageFactory.changeLanguage = function (lang, length, item1, item2){
+        if(lang==="fi"){
+          for(i=0;i<length;i++)
+          {
+            item1[i] = item2[i].en;
+          }
+        } else if(lang==="en"){
+          for(i=0;i<length;i++)
+          {
+            item1[i] = item2[i].fi;
+          } 
+        }
+    }
+    return languageFactory;
+});
+
+app.controller("translationCtrl", ["$scope", "$http", "$sce", "httpFactory", "heartFactory", "languageFactory", "localStorageFactory",
+  function($scope, $http, $sce, httpFactory, heartFactory, languageFactory, localStorageFactory) {
+  
+  $scope.skills = [];
+    $scope.texts = [];
+    $scope.section = [];
+    $scope.texts = [];
+    $scope.lang = "fi";
+    
+    var urlLength;
+    var text = [];
+    var url = [];
+    var isStorageAvailable = localStorageFactory.isStorageAvailable();
+
+    if(isStorageAvailable === true){
+    if(localStorage.getItem("language") !== null)
+    {
+      $scope.lang = localStorage.getItem("language");
+    } else
+    {
+      $scope.lang = "fi";
+    }
+    }else{
+        $scope.lang = "fi";
+    }
+
+    httpFactory.getData(function(data) {
+      $scope.skills = data.skills;
+      url = data.urls;
+      text = data.text;
+      urlLength = data.urls.length;
+      for(i=0;i<urlLength;i++)
+      {
+        if($scope.lang === "fi"){
+          $scope.section[i] = url[i].fi;
+        } else if ($scope.lang === "en") {
+          $scope.section[i] = url[i].en;
+        }
+      }
+      textLength = data.urls.length;
+      for(i=0;i<textLength;i++)
+      {
+        if($scope.lang === "fi"){
+          $scope.texts[i] = text[i].fi;
+        } else if ($scope.lang === "en") {
+          $scope.texts[i] = text[i].en;
+        }
+      }
+    });  
+
+    $scope.changeLanguage = function(lang){
+        languageFactory.changeLanguage(lang, urlLength, $scope.section, url);
+        languageFactory.changeLanguage(lang, textLength, $scope.texts, text);
+        if(lang==="fi"){
+          $scope.lang = "en";
+        } else if(lang==="en"){
+          $scope.lang = "fi";
+        }
+        if(isStorageAvailable === true){
+          localStorage.setItem("language", $scope.lang);
+        }
+    }
+    
+    
+    $scope.addHearts = function (index, value){
+       return heartFactory.addHearts(index, value);
+    }
+  
+  /*$scope.partials = [ { fin: "./partials/info_fi.html", en: "./partials/info_en.html"},
       { fin: "./partials/education_fi.html", en: "./partials/education_en.html"},
       { fin: "./partials/work1_fi.html", en: "./partials/work1_en.html"},
       { fin: "./partials/work2_fi.html", en: "./partials/work2_en.html"},
       { fin: "./partials/project_fi.html", en: "./partials/project_en.html"} ];
-  
+
   $scope.text = [{ fin: "Tietoa", en: "About Me"},
       { fin: "Koulutus", en: "Education"},
       { fin: "Työnäytteet", en: "Work"},
@@ -55,7 +186,7 @@ app.controller("translationCtrl", ["$scope", "localStorageFactory", function($sc
   }else{
       $scope.flag = true;
   }
-
+  
   $scope.getContent = function(){
     if($scope.flag===true)
     {
@@ -81,7 +212,7 @@ app.controller("translationCtrl", ["$scope", "localStorageFactory", function($sc
   }
   
   $scope.getContent();
-  
+    
   $scope.changeContent = function(){
       if($scope.flag===true)
       {
@@ -91,5 +222,5 @@ app.controller("translationCtrl", ["$scope", "localStorageFactory", function($sc
         $scope.flag = true;
       } 
       $scope.getContent();     
-  }
+  } */
 }]);
